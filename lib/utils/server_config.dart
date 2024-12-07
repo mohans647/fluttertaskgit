4b240e13696aa1ff1bf30defb23566ac8a1dc02a
Mohan <mohan@halsimplify.com>
2024-12-07 10:25:21 +0530

    version add

--- lib/utils/server_config.dart
+++ lib/utils/server_config.dart
@@ -1,5 +1,5 @@
 class Base_Url {
   // static const apiUrl = "http://43.205.142.22:8080/";
   static const Base_apiUrl = "https://api.github.com/repos/";
-  static const Githup_API= "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";
+  static const Githup_API= "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";
 }
